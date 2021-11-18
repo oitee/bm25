83f6e27cc8c4c7f0ac3eafc3e9475f13b6c0092e
Otee <oitee.codes@gmail.com>
2021-11-18 17:01:29 +0530

    Sort the documents containing query terms, as per relevance

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
+/**
+ * {object{object}} documentList
+ * To store, on a document level, the number of occurences of each word of every document.
+ */
 let documentList = {};
-let wordList = {};
-let idCounter = 0;
-let totalWordCount = 0;
-let listOfDocIds = {};
-let numberOfDocs = 0;
-let averageLength = totalWordCount / numberOfDocs;
-const K = 1.5;
-const B = 0.75;
-
-let aver;
-//constants
 
 /**
- 
- IDF = log ((N - n(qi) + 0.5)/ (n(qi) + 0.5) + 1)
- Math.log(n);
+ * {object{object}} wordList
+ * To store, on a word level, the number of documents each word is present
+ */
+let wordList = {};
 
- For each query term:
- score = (IDF) * (f(qi, doc) * (k + 1)) / ( f(qi, doc) + k + (1 - b + (b * |D| / averLength)))
+/**
+ * {integer} idCounter
+ * Counter to track id of each new document
+ *
+ */
+let idCounter = 0;
 
+/**
+ * {integer} totalWordCount
+ * To store the total number of words in all documents combined
+ */
+let totalWordCount = 0;
 
- }
+/**
+ * {object} listOfDocIds
+ * To store all document IDs and their respect documents, as key-value pairs
+ */
+let listOfDocIds = {};
 
- * 
+/**
+ * {integer} numberOfDocs
+ * To store the total number of documents
+ */
+let numberOfDocs = 0;
+
+/**
+ * {integer} averageLength
+ * To store the average length (in words) of all the documents
+ */
+let averageLength = totalWordCount / numberOfDocs;
+
+/**
+ * {integer} K
+ * Free parameter for BM25 algorithm
+ */
+const K = 1.5;
+
+/**
+ * {integer} B
+ * Free parameter for BM25 algorithm
+ */
+const B = 0.75;
+
+/**
+ *
+ * @param {string} str
+ * @returns {null}
+ * Inserts a new document in the system
  */
 
 function insert(str) {
@@ -55,6 +90,13 @@ function insert(str) {
   averageLength = totalWordCount / numberOfDocs;
 }
 
+/**
+ *
+ * @param {string} str
+ * @returns {array}
+ * Converts a string into an array of words
+ */
+
 function wordArray(str) {
   let strArr = [...str];
   let currentWord = "";
@@ -69,12 +111,18 @@ function wordArray(str) {
       }
     }
   });
-  if(currentWord.length > 0){
+  if (currentWord.length > 0) {
     words.push(currentWord);
   }
   return words;
 }
 
+/**
+ *
+ * @param {string} char
+ * @returns {boolean}
+ * Determines if a character is an alphabet/digit or not
+ */
 function isAlphabetOrDigit(char) {
   let currentCharCode = char.charCodeAt(0);
   if (
@@ -98,59 +146,68 @@ function isAlphabetOrDigit(char) {
   return false;
 }
 
+/**
+ *
+ * @param {string} queryStr
+ * @returns {null}
+ * Implements query search and logs the documents which contain the query string, in order of relevance
+ */
 function search(queryStr) {
   let queryTerms = wordArray(queryStr);
-  //console.log(queryTerms)
   let idfObject = idf(queryTerms);
   let documentsWithQuery = {};
   for (let docId in documentList) {
-    //console.log({docId});
-    // console.log(documentList[docId]);
     let docScore = score(documentList[docId], queryTerms, idfObject);
-    // console.log({docScore})
     if (docScore > 0) {
       documentsWithQuery[docId] = docScore;
     }
   }
-  console.log(documentsWithQuery);
+  // sorting the documents:
+  let sortedDocs = [];
+  for (let docId in documentsWithQuery) {
+    sortedDocs.push([docId, [documentsWithQuery[docId]]]);
+  }
+  sortedDocs.sort((a, b) => {
+    return b[1][0] - a[1][0];
+  });
+  for (let i = 0; i < sortedDocs.length; i++) {
+    let docId = sortedDocs[i][0];
+    console.log(`${i + 1}. ${listOfDocIds[docId]}`);
+  }
 }
 
+/**
+ *
+ * @param {string} doc
+ * @param {array} queryTerms
+ * @param {object} idf
+ * @returns {integer}
+ * Calculates the score of a given document, in context of a list of query terms
+ */
 function score(doc, queryTerms, idf) {
   let docScore = 0;
-  // console.log({doc});
-   //console.log({idf})
   queryTerms.map((query) => {
     let termFrequency = 0;
-    // console.log({doc});
-    // console.log({query})
-    // console.log(doc.hasOwnProperty(query));
-    // console.log(`-----------`)
-    
     if (doc.hasOwnProperty(query)) {
       termFrequency = doc[query];
     }
-    
+
     let wordsInDoc = Object.keys(doc).length;
-    // console.log({wordsInDoc});
-    // console.log({termFrequency});
-    // console.log(`idf [query] ${idf[query]}`);
-    // console.log({K});
-    // console.log({B});
-    // console.log({wordsInDoc});
-    // console.log({averageLength});
     docScore +=
       (idf[query] * (termFrequency * (K + 1))) /
       (termFrequency + K * (1 - B + (B * wordsInDoc) / averageLength));
-      // console.log({docScore})
   });
   return docScore;
 }
-
+/**
+ *
+ * @param {array} queryTerms
+ * @returns {object}
+ * Given a list of query terms, returns an object with containing the inverse document frequency of each document
+ */
 function idf(queryTerms) {
   let documentFrequency = {};
-  // console.log(queryTerms)
   queryTerms.map((query) => {
-    // console.log({query})
     let documentCount = 0;
     for (let docId in documentList) {
       if (documentList[docId].hasOwnProperty(query)) {
@@ -167,10 +224,8 @@ function idf(queryTerms) {
 
 insert("Hey There. This is document 1");
 insert("Hello! Document 2 says hi!");
-insert("Hi, Document 3 reporting!");
+insert("Hi, Document 3 reporting ot!");
 insert("NOPE; polar bear");
-//console.log({ listOfDocIds });
-// console.log({ wordList });
-//console.log(documentList);
-search("document there");
-search("polar");
+// search("document there");
+// search("polar");
+search("ot document");

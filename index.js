25db7152d28a10792fca1871b584106f24bacb5d
Otee <oitee.codes@gmail.com>
2021-11-23 17:02:35 +0530

    Refactor code: Use regex to split string into words, encapsulate functions into a new class

--- index.js
+++ index.js
@@ -1,231 +1,210 @@
-/**
- * {object{object}} documentList
- * To store, on a document level, the number of occurences of each word of every document.
- */
-let documentList = {};
+class DocStore {
+  constructor() {
+    /**
+     * {object{object}} documentList
+     * To store, on a document level, the number of occurences of each word of every document.
+     */
+    this.documentList = {};
 
-/**
- * {object{object}} wordList
- * To store, on a word level, the number of documents each word is present
- */
-let wordList = {};
+    /**
+     * {object{object}} wordList
+     * To store, on a word level, the number of documents each word is present
+     */
+    this.wordList = {};
 
-/**
- * {integer} idCounter
- * Counter to track id of each new document
- *
- */
-let idCounter = 0;
+    /**
+     * {integer} idCounter
+     * Counter to track id of each new document
+     *
+     */
+    this.idCounter = 0;
 
-/**
- * {integer} totalWordCount
- * To store the total number of words in all documents combined
- */
-let totalWordCount = 0;
+    /**
+     * {integer} totalWordCount
+     * To store the total number of words in all documents combined
+     */
+    this.totalWordCount = 0;
 
-/**
- * {object} listOfDocIds
- * To store all document IDs and their respect documents, as key-value pairs
- */
-let listOfDocIds = {};
+    /**
+     * {object} listOfDocIds
+     * To store all document IDs and their respect documents, as key-value pairs
+     */
+    this.listOfDocIds = {};
 
-/**
- * {integer} numberOfDocs
- * To store the total number of documents
- */
-let numberOfDocs = 0;
+    /**
+     * {integer} numberOfDocs
+     * To store the total number of documents
+     */
+    this.numberOfDocs = 0;
 
-/**
- * {integer} averageLength
- * To store the average length (in words) of all the documents
- */
-let averageLength = totalWordCount / numberOfDocs;
+    /**
+     * {integer} averageLength
+     * To store the average length (in words) of all the documents
+     */
+    this.averageLength = this.totalWordCount / this.numberOfDocs;
 
-/**
- * {integer} K
- * Free parameter for BM25 algorithm
- */
-const K = 1.5;
+    /**
+     * {integer} K
+     * Free parameter for BM25 algorithm
+     */
+    this.K = 1.5;
 
-/**
- * {integer} B
- * Free parameter for BM25 algorithm
- */
-const B = 0.75;
+    /**
+     * {integer} B
+     * Free parameter for BM25 algorithm
+     */
+    this.B = 0.75;
+  }
+  /**
+   *
+   * @param {string} str
+   * @returns {null}
+   * Inserts a new document in the system
+   */
 
-/**
- *
- * @param {string} str
- * @returns {null}
- * Inserts a new document in the system
- */
+  insert(str) {
+    str = str.toLowerCase();
+    this.idCounter++;
+    let docId = this.idCounter;
+    this.listOfDocIds[docId] = str;
+    let words = this.wordArray(str);
+    words.map((word) => {
+      if (this.wordList.hasOwnProperty(word)) {
+        this.wordList[word]++;
+      } else {
+        this.wordList[word] = 1;
+      }
+    });
+    let listOfWordsInThisDoc = {};
+    let wordCount = 0;
+    words.map((word) => {
+      if (listOfWordsInThisDoc.hasOwnProperty(word)) {
+        listOfWordsInThisDoc[word]++;
+      } else {
+        listOfWordsInThisDoc[word] = 1;
+      }
+      wordCount++;
+    });
 
-function insert(str) {
-  str = str.toLowerCase();
-  idCounter++;
-  let docId = idCounter;
-  listOfDocIds[docId] = str;
-  let words = wordArray(str);
-  words.map((word) => {
-    if (wordList.hasOwnProperty(word)) {
-      wordList[word]++;
-    } else {
-      wordList[word] = 1;
+    this.documentList[docId] = listOfWordsInThisDoc;
+    this.totalWordCount += wordCount;
+    this.numberOfDocs++;
+    this.averageLength = this.totalWordCount / this.numberOfDocs;
+  }
+
+  /**
+   *
+   * @param {string} str
+   * @returns {array}
+   * Converts a string into an array of words
+   */
+
+  wordArray(str) {
+    let regex = /[\W]+/;
+    let words = str.split(regex);
+    if (words[words.length - 1] === "") {
+      words.pop();
     }
-  });
-  let listOfWordsInThisDoc = {};
-  let wordCount = 0;
-  words.map((word) => {
-    if (listOfWordsInThisDoc.hasOwnProperty(word)) {
-      listOfWordsInThisDoc[word]++;
-    } else {
-      listOfWordsInThisDoc[word] = 1;
+    return words;
+  }
+
+  /**
+   *
+   * @param {string} queryStr
+   * @returns {null}
+   * Implements query search and logs the documents which contain the query string, in order of relevance
+   */
+  search(queryStr) {
+    let queryTerms = this.wordArray(queryStr);
+    let idfObject = this.idf(queryTerms);
+    if(Object.keys(idfObject).length === 0){
+      console.log("No Matching results");
+      return;
     }
-    wordCount++;
-  });
-
-  documentList[docId] = listOfWordsInThisDoc;
-  totalWordCount += wordCount;
-  numberOfDocs++;
-  averageLength = totalWordCount / numberOfDocs;
-}
-
-/**
- *
- * @param {string} str
- * @returns {array}
- * Converts a string into an array of words
- */
-
-function wordArray(str) {
-  let strArr = [...str];
-  let currentWord = "";
-  let words = [];
-  strArr.map((char) => {
-    if (isAlphabetOrDigit(char)) {
-      currentWord += char;
-    } else {
-      if (currentWord.length > 0) {
-        words.push(currentWord);
-        currentWord = "";
+    let documentsWithQuery = {};
+    for (let docId in this.documentList) {
+      let docScore = this.score(
+        this.documentList[docId],
+        queryTerms,
+        idfObject
+      );
+      if (docScore > 0) {
+        documentsWithQuery[docId] = docScore;
+      }
+      if(Object.keys(documentsWithQuery).length === 0){
+        console.log(`No matching result for the query: ${queryStr}`);
+        return;
       }
     }
-  });
-  if (currentWord.length > 0) {
-    words.push(currentWord);
-  }
-  return words;
-}
-
-/**
- *
- * @param {string} char
- * @returns {boolean}
- * Determines if a character is an alphabet/digit or not
- */
-function isAlphabetOrDigit(char) {
-  let currentCharCode = char.charCodeAt(0);
-  if (
-    currentCharCode >= "a".charCodeAt(0) &&
-    currentCharCode <= "z".charCodeAt(0)
-  ) {
-    return true;
-  }
-  if (
-    currentCharCode >= "A".charCodeAt(0) &&
-    currentCharCode <= "Z".charCodeAt(0)
-  ) {
-    return true;
-  }
-  if (
-    currentCharCode >= "0".charCodeAt(0) &&
-    currentCharCode <= "9".charCodeAt(0)
-  ) {
-    return true;
-  }
-  return false;
-}
-
-/**
- *
- * @param {string} queryStr
- * @returns {null}
- * Implements query search and logs the documents which contain the query string, in order of relevance
- */
-function search(queryStr) {
-  let queryTerms = wordArray(queryStr);
-  let idfObject = idf(queryTerms);
-  let documentsWithQuery = {};
-  for (let docId in documentList) {
-    let docScore = score(documentList[docId], queryTerms, idfObject);
-    if (docScore > 0) {
-      documentsWithQuery[docId] = docScore;
+    // sorting the documents:
+    let sortedDocs = [];
+    for (let docId in documentsWithQuery) {
+      sortedDocs.push([docId, [documentsWithQuery[docId]]]);
+    }
+    sortedDocs.sort((a, b) => {
+      return b[1][0] - a[1][0];
+    });
+    console.log(`Matching results for the query: ${queryStr}`);
+    for (let i = 0; i < sortedDocs.length; i++) {
+      let docId = sortedDocs[i][0];
+      console.log(`${i + 1}. ${this.listOfDocIds[docId]}`);
     }
   }
-  // sorting the documents:
-  let sortedDocs = [];
-  for (let docId in documentsWithQuery) {
-    sortedDocs.push([docId, [documentsWithQuery[docId]]]);
-  }
-  sortedDocs.sort((a, b) => {
-    return b[1][0] - a[1][0];
-  });
-  for (let i = 0; i < sortedDocs.length; i++) {
-    let docId = sortedDocs[i][0];
-    console.log(`${i + 1}. ${listOfDocIds[docId]}`);
-  }
-}
 
-/**
- *
- * @param {string} doc
- * @param {array} queryTerms
- * @param {object} idf
- * @returns {integer}
- * Calculates the score of a given document, in context of a list of query terms
- */
-function score(doc, queryTerms, idf) {
-  let docScore = 0;
-  queryTerms.map((query) => {
-    let termFrequency = 0;
-    if (doc.hasOwnProperty(query)) {
-      termFrequency = doc[query];
-    }
-
-    let wordsInDoc = Object.keys(doc).length;
-    docScore +=
-      (idf[query] * (termFrequency * (K + 1))) /
-      (termFrequency + K * (1 - B + (B * wordsInDoc) / averageLength));
-  });
-  return docScore;
-}
-/**
- *
- * @param {array} queryTerms
- * @returns {object}
- * Given a list of query terms, returns an object with containing the inverse document frequency of each document
- */
-function idf(queryTerms) {
-  let documentFrequency = {};
-  queryTerms.map((query) => {
-    let documentCount = 0;
-    for (let docId in documentList) {
-      if (documentList[docId].hasOwnProperty(query)) {
-        documentCount++;
+  /**
+   *
+   * @param {string} doc
+   * @param {array} queryTerms
+   * @param {object} idf
+   * @returns {integer}
+   * Calculates the score of a given document, in context of a list of query terms
+   */
+  score(doc, queryTerms, idf) {
+    let docScore = 0;
+    queryTerms.map((query) => {
+      let termFrequency = 0;
+      if (doc.hasOwnProperty(query)) {
+        termFrequency = doc[query];
       }
-    }
-    documentFrequency[query] = Math.log(
-      (totalWordCount - documentCount + 0.5) / (documentCount + 0.5) + 1
-    );
-    return;
-  });
-  return documentFrequency;
+
+      let wordsInDoc = Object.keys(doc).length;
+      docScore +=
+        (idf[query] * (termFrequency * (this.K + 1))) /
+        (termFrequency +
+          this.K * (1 - this.B + (this.B * wordsInDoc) / this.averageLength));
+    });
+    return docScore;
+  }
+  /**
+   *
+   * @param {array} queryTerms
+   * @returns {object}
+   * Given a list of query terms, returns an object with containing the inverse document frequency of each document
+   */
+  idf(queryTerms) {
+    let documentFrequency = {};
+    queryTerms.map((query) => {
+      let documentCount = 0;
+      for (let docId in this.documentList) {
+        if (this.documentList[docId].hasOwnProperty(query)) {
+          documentCount++;
+        }
+      }
+      documentFrequency[query] = Math.log(
+        (this.totalWordCount - documentCount + 0.5) / (documentCount + 0.5) + 1
+      );
+      return;
+    });
+    return documentFrequency;
+  }
 }
 
-insert("Hey There. This is document 1");
-insert("Hello! Document 2 says hi!");
-insert("Hi, Document 3 reporting ot!");
-insert("NOPE; polar bear");
-// search("document there");
-// search("polar");
-search("ot document");
+let newDocuments = new DocStore();
+newDocuments.insert("Hey There. This is document 1");
+newDocuments.insert("Hello! Document 2 says hi!");
+newDocuments.insert("Hi, Document 3 reporting ot!");
+newDocuments.insert("NOPE; polar bear");
+newDocuments.search("document there");
+newDocuments.search("polar");
+newDocuments.search("ot document");
+newDocuments.search("fibonacci");
